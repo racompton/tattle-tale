d966b24ad4e6eccabf4818f51f044461129bd7cb
Craig Pratt <c.pratt@cablelabs.com>
2021-07-28 02:21:26 -0700

    Normalizing the paths to enable the use of storage volume for shared data

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,15 @@ WORKDIR /opt/tattle-tail
 COPY requirements.txt ./
 COPY *.py bin/
 
+RUN mkdir -pv /opt/tattle-tail/{var/downloads,lib/logstash,etc,var/tmp}
+
 RUN apt-get update
 RUN apt-get -y install libsmi-dev gcc curl
 
 RUN curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.13.4-amd64.deb
 RUN dpkg -i filebeat-7.13.4-amd64.deb
+
+COPY filebeat/* /etc/filebeat/
 
 # Install any needed packages specified in requirements.txt
 RUN pip3 install --trusted-host pypi.python.org -r requirements.txt
--- tattle_tale_cfg.py
+++ tattle_tale_cfg.py
@@ -11,18 +11,18 @@ shadow_url = "https://dl.shadowserver.org/reports/index.php"
 
 # Directory to store Shadowserver downloads
 # Must include the trailing /
-shadow_dir = "/opt/tattle_tale/downloads/"
+shadow_dir = "/opt/tattle-tale/var/downloads/"
 
 # Directory to store new dictionaries
 # Must include the trailing /
-shadow_temp_dir = "/opt/tattle_tale/dicts/"
+shadow_temp_dir = "/opt/tattle-tale/var/tmp/"
 
 # Dictionary location for Logstash
 # Must include the trailing /
-logstash_dict_dir = "/etc/logstash/dictionaries/"
+logstash_dict_dir = "/opt/tattle-tale/lib/logstash/"
 
 # File containing list of IP addresses to query via SNMP
-device_list = "/opt/tattle_tale/router_list.txt"
+device_list = "/opt/tattle-tale/etc/router-list.txt"
 
 # SNMPv2c community string
 snmp_community = "<community string>"
