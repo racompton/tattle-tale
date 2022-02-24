8fe093cffdec556d2b47938007d42ac7e7445a50
Rich Compton <rich.compton@charter.com>
2022-02-23 17:14:33 -0800

    Update and rename delete_old_indicies.sh to delete_old_indices.sh

--- cron.daily/delete_old_indices.sh
+++ cron.daily/delete_old_indices.sh
@@ -3,4 +3,4 @@
 # Script to delete elasticsearch netflow-* indicies older than 45 days
 # Put this file in /etc/cron.daily/ and make sure it's executable (chmod 755 delete_old_indicies.sh)
 
-/bin/curator /etc/curator/delete_old_indicies.yml --config /etc/curator/curator.yml 
+/bin/curator /etc/curator/delete_old_indices.yml --config /etc/curator/curator.yml 
