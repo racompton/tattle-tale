0955179f1469d56a314f13da0136edec2b1f0ca4
Rich Compton <rich.compton@charter.com>
2023-02-14 14:34:36 -0700

    Update tattle_tale_shadow.sh

--- cron.daily/tattle_tale_shadow.sh
+++ cron.daily/tattle_tale_shadow.sh
@@ -3,7 +3,7 @@
 # Run the script to pull down the shadowserver files and then create dictionary files for each
 # The logstash filters then use this to filter out events
 
-/opt/tattle_tale/tattle_shadow.py
+/opt/tattle_tale/download-shadowserver-reports.py
 
 # Generate a dictionary file for the entries from dissarm.net for the last day
 # /opt/tattle_tale/get-dissarm-ips.py -k <API key> -o <Org ID> -d 1 -f /etc/logstash/dictionaries/dis-abusesrs.yml
