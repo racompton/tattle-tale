85e1384193772a11f1f837c81a7e5669883f92f7
Rich Compton <richard.compton@gmail.com>
2024-09-25 11:34:19 -0600

    Update tattle_tale_shadow.sh

--- cron.daily/tattle_tale_shadow.sh
+++ cron.daily/tattle_tale_shadow.sh
@@ -5,5 +5,8 @@
 
 /opt/tattle-tale/download-shadowserver-reports.py
 
-# Generate a dictionary file for the entries from dissarm.net for the last day
+# Generate a dictionary file (dis.yml) for the entries from dissarm.net for the last day
 # /opt/tattle_tale/get-dissarm-ips.py -k <API key> -o <Org ID> -d 1 -f /etc/logstash/dictionaries/dis-abusesrs.yml
+
+# Generate a dictionary file (ifName.yml) for the interface names by SNMP polling the routers
+# /opt/tattle_tale/tattle_snmp_poll.py 
